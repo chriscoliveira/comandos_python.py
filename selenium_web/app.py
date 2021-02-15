0a63c7d4aba0e2144d18b93c88f602b392eaaf66
Christian Carvalho <christian.coliveira@gmail.com>
2021-02-15 19:15:28 -0300

    selenium web

--- selenium_web/app.py
+++ selenium_web/app.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from time import sleep
 
+
 class ChromeAuto:
     def __init__(self):
-        self.driver_path = 'chromedriver.exe'
+        self.driver_path = 'chromedriver'
         self.options = webdriver.ChromeOptions()
         self.options.add_argument('user-data-dir=Perfil')
         self.chrome = webdriver.Chrome(
@@ -11,36 +12,72 @@ class ChromeAuto:
             options=self.options
         )
 
-    def acessa(self,site):
+    def clica_sign_in(self):
+        try:
+            btn_sign_in = self.chrome.find_element_by_link_text('Sign in')
+            btn_sign_in.click()
+        except Exception as e:
+            print('Erro ao clicar em Sign in:', e)
+
+    def acessa(self, site):
         self.chrome.get(site)
 
     def sair(self):
         self.chrome.quit()
 
-    def clica_singin(self):
+    def clica_perfil(self):
         try:
-            btn_singin = self.chrome.find_element_by_link_text('Sign in')
-            btn_singin.click()
+            # perfil = self.chrome.find_element_by_css_selector(
+            #     'body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-md-flex > details')
+            perfil = self.chrome.find_element_by_css_selector(
+                'body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-lg-flex > details')
+            # perfil = self.chrome.find_element_by_class_name('details-overlay')
+            perfil.click()
         except Exception as e:
-            print('Erro ao clicar: '+e)
+            print('Erro ao clicar no perfil:', e)
 
-    def fazer_login(self):
+    def faz_logout(self):
+        try:
+            perfil = self.chrome.find_element_by_css_selector(
+                'body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-lg-flex > details > details-menu > form > button')
+
+            perfil.click()
+        except Exception as e:
+            print('Erro fazer logout:', e)
+
+    def verifica_usuario(self, usuario):
+        profile_link = self.chrome.find_element_by_class_name(
+            'user-profile-link')
+        profile_link_html = profile_link.get_attribute('innerHTML')
+        assert usuario in profile_link_html
+
+    def faz_login(self):
         try:
             input_login = self.chrome.find_element_by_id('login_field')
             input_password = self.chrome.find_element_by_id('password')
             btn_login = self.chrome.find_element_by_name('commit')
+
             input_login.send_keys('chriscoliveira')
             input_password.send_keys('H4a9YeyBXNYQzkd')
-            sleep(2)
+
             btn_login.click()
+
         except Exception as e:
-            print('Erro ao fazer login: ' + e)
+            print('Erro ao fazer login:', e)
+
 
 if __name__ == '__main__':
     chrome = ChromeAuto()
-    chrome.acessa('https://github.com')
-    chrome.clica_singin()
-    chrome.fazer_login()
-    sleep(30)
+    chrome.acessa('https://github.com/')
 
+    # chrome.clica_perfil()
+    # chrome.faz_logout()
+    #
+    chrome.clica_sign_in()
+    chrome.faz_login()
+
+    # chrome.clica_perfil()
+    # chrome.verifica_usuario('chriscoliveira')
+
+    sleep(20)
     chrome.sair()
